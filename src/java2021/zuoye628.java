07f50513e72e262c830c3d4bc2ce471773e2a31a
764642764 <764642764@qq.com>
2020-06-28 12:02:08 +0800

    123

--- src/java2021/zuoye628.java
+++ src/java2021/zuoye628.java
@@ -5,13 +5,23 @@ public class zuoye628 extends Thread {
 		for (int i = 0; i < 5; i++) {
 			System.out.println(i + "+++ 多线程+++");
 		}
+		
 	}
 
 	public static void main(String[] args) {
 		zuoye628 thread = new zuoye628();
+		One one = new One();
+		one.start();
 		thread.start();
 		for (int i = 0; i < 5; i++) {
 			System.out.println(i + "---主线程---");
+		}
+	}
+}
+class One extends Thread{
+	public void run() {
+		for(int i=0;i<5;i++) {
+			System.out.println(i+"aling***");
 		}
 	}
 }
