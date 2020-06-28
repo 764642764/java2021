c5501127f68d6d4e79121de876ff2ba4290d6589
764642764 <764642764@qq.com>
2020-06-28 11:28:13 +0800

    多线程

---
+++ src/java2021/zuoye628.java
@@ -0,0 +1,19 @@
+package java2021;
+public class zuoye628  extends Thread{
+    public void run()
+    {
+    	for(int i=0;i<5;i++)
+    	{
+    		System.out.println(i+"+++ 多线程+++");
+    	}
+    }
+    public static void main(String[] args)
+    {
+    	zuoye628 thread=new zuoye628();
+    	thread.start();
+    	for(int i=0;i<5;i++)
+    	{
+    		System.out.println(i+"---主线程---");
+    	}
+    }
+}
